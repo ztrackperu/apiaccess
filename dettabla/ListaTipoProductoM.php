d20686ca38156413ec5d516911d6a1094c15556b
ztrackperu <ztrack@zgroup.com.pe>
2024-04-08 18:38:34 -0500

    pa prueba4

--- dettabla/ListaTipoProductoM.php
+++ dettabla/ListaTipoProductoM.php
@@ -41,7 +41,7 @@ try {
 $total =[];
 //depuracion para obtener C_NUMITM ,C_DESITM
 //$cursor  = $client->intranet->dettabla->find(array('c_codtab'=>'CLP'),array('projection' => array('C_NUMITM'=> 1, 'C_DESITM' => 1),'sort'=>array('_id'=>-1),'limit'=>50000));
-$cursor  = $client->intranet->dettabla->find();
+$cursor  = $client->intranet->dettabla->find(array('c_codtab'=>'CLP'));
 
 foreach ($cursor as $document) {
     array_unshift($total,$document);
