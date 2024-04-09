fa0ca58714c57c2f29c3a0674796c76976e142b5
ztrackperu <ztrack@zgroup.com.pe>
2024-04-09 11:43:23 -0500

    tec2

--- mongodb/mongo.php
+++ mongodb/mongo.php
@@ -7,11 +7,11 @@ use MongoDB\Client;
 use MongoDB\Driver\ServerApi;
 use MongoDB\BSON\UTCDateTime ;
 
-$uri = 'mongodb://localhost:27017';
+const uri = 'mongodb://localhost:27017';
 // Specify Stable API version 1
-$apiVersion = new ServerApi(ServerApi::V1);
+const apiVersion = new ServerApi(ServerApi::V1);
 // Create a new client and connect to the server
-const client = new MongoDB\Client($uri, [], ['serverApi' => $apiVersion]);
+const client = new MongoDB\Client(uri, [], ['serverApi' => apiVersion]);
 
 
 
