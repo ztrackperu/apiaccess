ac26d1c6e0dd612d1cc24a408933bf14e8c74b8a
ztrackperu <ztrack@zgroup.com.pe>
2024-04-09 14:22:38 -0500

    tec5

--- mongodb/mongo.php
+++ mongodb/mongo.php
@@ -6,6 +6,7 @@ require '../../ztotal/vendor/autoload.php';
 use MongoDB\Client;
 use MongoDB\Driver\ServerApi;
 use MongoDB\BSON\UTCDateTime ;
+use MongoDB\BSON\Regex ;
 
 const uri = 'mongodb://localhost:27017';
 // Specify Stable API version 1
@@ -13,6 +14,15 @@ const apiVersion = new ServerApi(ServerApi::V1);
 // Create a new client and connect to the server
 const client = new MongoDB\Client(uri, [], ['serverApi' => apiVersion]);
 
+//db.madurador.explain("executionStats").find()
+//db.madurador.find().sort({id:-1}).limit(1)
 
+//db.madurador.explain("executionStats").find().sort({id:-1}).limit(1)
+/*
+$regexObj = new MongoRegex("/^Nicolas/");
+db.invmae.explain("executionStats").find({"IN_CODI": /^INDND/}).sort({id:-1}).limit(1)
+db.invmae.explain("executionStats").find({"IN_CODI": /^INDND/}).sort({IN_CODI:1}).limit(1)
+db.invmae.find({"IN_CODI": /^INDND/}).sort({"IN_CODI":-1}).projection({'_id':0,"IN_CODI":1}).limit(1)
+*/
 
 ?>
